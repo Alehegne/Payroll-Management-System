0b4fa7147881ac5d926c4813ace7dc961b12e1a3
Alehegne <alehegngetaenyewuu@gmail.com>
2024-12-27 18:54:28 +0300

    declaring variables and adding more features to the main functions

--- main.cpp
+++ main.cpp
@@ -1,43 +1,45 @@
 #include <iostream>
 using namespace std;
 
-void addEmployee()
-{
-    cout << "Add Employee" << endl;
-}
-
-void viewEmployee()
-{
-    cout << "View Employee" << endl;
-}
-
-void searchEmployee()
-{
-    cout << "Search Employee" << endl;
-}
-void viewSummaryReport()
-{
-    cout << "View Summary Report" << endl;
-}
+// Function prototypes
+void addEmployee();
+void viewEmployee();
+void searchEmployee();
+void viewSummaryReport();
 
 int main()
 {
 
     string names[100];
     char gender[100];
-    string employeType[100];
+    string employe_Type[100];
+    double hoursWorked[100];
+    double salary[100];
 
-    cout << "Payroll Management System" << endl;
-
-    cout << "1. Add Employee" << endl;
-    cout << "2.view Employee" << endl;
-    cout << "3.search Employee" << endl;
-    cout << "4.view summary report" << endl;
-    cout << "5.exit" << endl;
+    int worker_count = 0; // to keep track of the number of workers
 
     int choice;
-    cout << "Enter your choice: ";
-    cin >> choice;
+
+    // Menu
+    do
+    {
+        cout << endl;
+        cout << "******************************************************************************************" << endl;
+        cout << "Payroll Management System" << endl;
+        cout << "1. Add Employee" << endl;
+        cout << "2.view Employee" << endl;
+        cout << "3.search Employee" << endl;
+        cout << "4.view summary report" << endl;
+        cout << "5.exit" << endl;
+        cout << "******************************************************************************************" << endl;
+        // User input
+        cout << "Enter your choice: ";
+        cin >> choice;
+        if (choice < 1 || choice > 5)
+        {
+            cout << "Invalid choice" << endl;
+        }
+    } while (choice < 1 || choice > 5);
 
     switch (choice)
     {
@@ -61,4 +63,26 @@ int main()
     }
 
     return 0;
+}
+
+void addEmployee()
+{
+    // update the worker_count while adding a new worker
+
+    cout << "Add Employee" << endl;
+}
+
+void viewEmployee()
+{
+
+    cout << "View Employee" << endl;
+}
+
+void searchEmployee()
+{
+    cout << "Search Employee" << endl;
+}
+void viewSummaryReport()
+{
+    cout << "View Summary Report" << endl;
 }
